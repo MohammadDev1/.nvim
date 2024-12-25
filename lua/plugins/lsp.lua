f454f40a940cca65a0f3c021e923f102ca55b6bb
MohammadDev1 <amrymhmd74@gmail.com>
2024-12-25 23:22:13 +0330

    Add glsl lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -177,6 +177,7 @@ return {
         
         require("lspconfig")["rust_analyzer"].setup({})
         require("lspconfig")["ts_ls"].setup({})
+        require("lspconfig")["glsllls"].setup({})
 
         lsp_zero.set_sign_icons({
             error = "✘",
