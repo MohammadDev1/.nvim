e7973ed14eb76c825c70ef4f34794adafd2c80e9
MohammadDev1 <amrymhmd74@gmail.com>
2025-04-10 19:16:44 +0330

    some changes to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,7 +4,14 @@ return {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function ()
-        require("oil").setup({})
+        require("oil").setup({
+            delete_to_trash = true,
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+            }
+
+        })
         vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
 }
